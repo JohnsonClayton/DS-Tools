86a3cfcd22a7434f222ee1797faca16acd23edb6
notclaytonjohnson <notclaytonjohnson@pop-os.localdomain>
2020-03-14 15:09:07 -0600

    completed IB1 classifier

---
+++ classifiers/IB1.java
@@ -0,0 +1,82 @@
+/*  Instance-based Classifier 1 created by Clayton Johnson
+ *    as described in Aha's Paper: Instance-Based Learning Algorithm
+ *
+ *
+ *   IB-1:
+ *   ------------------------
+ *   CD = null_set
+ *   for each x in x_train do 
+ *     1. for each y_train do
+ *       Sim[y] <- Similarity(x, y)
+ *     2. y_max <- some y in CD with maximal Sim[y]
+ *     3. if class(x) == class(y_max)
+ *        then classification <- correct
+ *        else classification <- incorrect
+ *     4. CD <- CD unioned with {x}
+ *
+ *    
+ *  This classifier matches the performance of IBk, k=1 and essentially a NearestNeighbor algorithm
+ *  Example that was followed: https://waikato.github.io/weka-blog/posts/2018-10-08-making-a-weka-classifier/
+ *
+ */
+
+package weka.classifiers.lazy;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import weka.classifiers.AbstractClassifier;
+import weka.classifiers.UpdateableClassifier;
+import weka.classifiers.rules.ZeroR;
+import weka.core.AdditionalMeasureProducer;
+import weka.core.Attribute;
+import weka.core.Capabilities;
+import weka.core.Capabilities.Capability;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.Option;
+import weka.core.OptionHandler;
+import weka.core.RevisionUtils;
+import weka.core.SelectedTag;
+import weka.core.Tag;
+import weka.core.TechnicalInformation;
+import weka.core.TechnicalInformation.Field;
+import weka.core.TechnicalInformation.Type;
+import weka.core.TechnicalInformationHandler;
+import weka.core.Utils;
+import weka.core.WeightedInstancesHandler;
+import weka.core.neighboursearch.LinearNNSearch;
+import weka.core.neighboursearch.NearestNeighbourSearch;
+
+public class IB1 extends AbstractClassifier {
+	protected int m_K = 1;
+	protected NearestNeighbourSearch m_NNSearch = new LinearNNSearch();
+
+	public void buildClassifier(Instances trainingData) throws Exception {
+		trainingData = new Instances(trainingData);
+		trainingData.deleteWithMissingClass();
+
+		m_NNSearch.setInstances(trainingData);
+	}
+
+	public double[] distributionForInstance(Instance testInstance) throws Exception {
+		m_NNSearch.addInstanceInfo(testInstance);
+
+		Instances neighbours = m_NNSearch.kNearestNeighbours(testInstance, m_K);
+
+		double[] dist = new double[testInstance.numClasses()];
+		for (Instance neighbour : neighbours) {
+			if (testInstance.classAttribute().isNominal()) {
+				dist[(int)neighbour.classValue()] += 1.0 / neighbours.numInstances();
+			} else {
+				dist[0] += neighbour.classValue() / neighbours.numInstances();
+			}
+		}
+		return dist;
+
+	}
+
+
+
+}
