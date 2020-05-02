e2a16ebe9f471a9a37e86af8b4822e8a19f9f336
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-05-02 11:17:23 -0600

    updated to evaluate the given learner automatically

--- stockpred.py
+++ stockpred.py
@@ -20,6 +20,13 @@ Close =
 
 """
 
+def get_signed_string(val):
+  if val > 0 :
+    return '+{:.2f}'.format(val)
+  else:
+    return '{:.2f}'.format(val)
+
+
 def predict_close_with_open(df, open_price=-1.0):
   # Find the lagged values
   lg1 = df['Close'][-1]
@@ -31,7 +38,39 @@ def predict_close_with_open(df, open_price=-1.0):
   # Project and return the closing price with the predefined coefficients
   close_price = (0.9141*open_price) + (0.0974*lg1) + (-0.052*lg6) + (0.1038*lg7) + (-0.1445*lg8) + (0.0805*lg9) + 0.1647
   return close_price
+
+def evaluate_learner(df, days):
+  error_total = 0
+
+  #print(df.tail(10))
+  print('\t\t\t\tPredicted\tReal')
+  print('Date\t\t\tOpen\tClose\tDelta\tClose\tDelta\tError')
+  for i in range(days, 0, -1):
+    #print('-------------------------')
+    data_today = df.tail(i).head(1)
+    data_history = df.head( len(df.index)-i )
+
+    #print('Last of larger set:')
+    #print(data_history.tail(1))
+    #print('Next day:')
+    #print(data_today)
+
+    # Get the data and predicted data for today
+    date = data_today.index[0]
+    open_today = data_today['Open'][0]
+    close_pred = predict_close_with_open(data_history, open_today)
+    delta_pred = close_pred - open_today
+    close_real = data_today['Close'][0]
+    delta_real = close_real - open_today
+    error = close_pred - close_real
+    error_total += error
   
+    print('{}\t{:.2f}\t{:.2f}\t{}\t{:.2f}\t{}\t{}'.format(date, open_today, close_pred, get_signed_string(delta_pred), close_real, get_signed_string(delta_real), get_signed_string(error)))
+  print('\t\t\t\t\t\t\t\t{:.2f}'.format(error_total))
+
+  #if error_total < 0:
+  #  print('The model is 
+
   
 if __name__ == '__main__':
   # Import the arff file
@@ -60,3 +99,6 @@ if __name__ == '__main__':
   # Output projected closing price
   print('The \'ol wise one predicts....')
   print('\tApple will close on {} ({}) today.'.format(pred_close, '+' + str(pred_close-open_price) if pred_close - open_price > 0 else pred_close-open_price)) 
+
+  print('Evaluating the learner...')
+  evaluate_learner(df, 10)
